f68fa8893685a3cc32b8f48f0abbb18282818fcf
LiubovAnufriieva <lyuzika@gmail.com>
2024-03-08 14:25:37 +0200

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,45 +1,20 @@
-const getUserNames = users => users.map(user => user.name);
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
-console.log(
-  getUserNames([
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    balance: 2764
-  },
-])
-); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+// На що буде звертати увагу ментор при перевірці:
+// Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
+// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
 
 
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('li.item');
 
+console.log(`Number of categories: ${categoriesItems.length}`);
 
+categoriesItems.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryItemsCount = category.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryName} 
+  Elements: ${categoryItemsCount}`);
+});
